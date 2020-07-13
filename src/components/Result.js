b20ff9fcef4266850df7c0d7b29c91482bc7f0f8
Kiana Alikhademi <kalikhademi@users.noreply.github.com>
2020-07-13 10:40:55 -0400

    updated the code

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,17 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
 import addCssTransition from "../utils/css-transition";
-// import { CSSTransitionGroup } from "react-transition-group";
-import { withStyles, MuiThemeProvider } from "@material-ui/core/styles";
+import { MuiThemeProvider } from "@material-ui/core/styles";
 import theme from "../styles/theme";
 import successSvg from "../svg/success.svg";
 import failureSvg from "../svg/failure.svg";
-import resultstyles from "../styles/result-style";
-import App from "../App";
+import styles from "../styles/result-style";
+// import App from "../App";
 
 function Result(props) {
   //change this based on ben's response about
-  const Button = resultstyles.resetButton;
+  // const Button = styles.resetButton;
+  const rStyle = styles(theme);
   var risk = false;
   if (props.quizResult > 70) {
     risk = true;
@@ -19,59 +19,32 @@ function Result(props) {
   return (
     <MuiThemeProvider theme={theme}>
       {addCssTransition(
-        <div className="">
-          <img
+        <div className={rStyle.svgContainer}>
+          {/* <img
             src={risk ? failureSvg : successSvg}
-            className={props.resultSVG}
+            className={rStyle.resultSVG}
             alt="logo"
-          />
-          The participant has <b>{risk ? "high" : "low"}</b> risk score.
-          <Button
+          /> */}
+          <p className={rStyle.resultParagraph}>
+            The total risk score is {props.quizResult}
+          </p>
+
+          {/* <Button
             className={props.resetButton}
             onClick={App.resetQuiz}
             color="secondary"
           >
             Try Again
-          </Button>
+          </Button> */}
         </div>
       )}
     </MuiThemeProvider>
   );
 }
-// <CSSTransitionGroup
-//   className="container result"
-//   component="div"
-//   transitionName="fade"
-//   transitionEnterTimeout={800}
-//   transitionLeaveTimeout={500}
-//   transitionAppear
-//   transitionAppearTimeout={500}
-// >
-
-/* <div>
-        <h1>Results</h1>
-        <p className=".result.single">The total risk is {props.quizResult}</p>
-        <p className=".result.protective">
-          The protective risk score is {props.protectiveResult}
-        </p>
-      </div> */
-
-// <div className="score-board">
-//   <div className="score">
-//     {" "}
-//     <p>Your risk score is {props.quizResult}</p>{" "}
-//     <p>and the protective result is {props.protectiveResult}</p>
-//   </div>
-//   <button className="playBtn" onClick={App.resetQuiz}>
-//     {" "}
-//     Start Again{" "}
-//   </button>
-// </div>
-// </CSSTransitionGroup>
-
 Result.propTypes = {
   quizResult: PropTypes.number.isRequired,
   protectiveResult: PropTypes.number.isRequired,
+  chresult: PropTypes.number.isRequired,
 };
 
 export default Result;
